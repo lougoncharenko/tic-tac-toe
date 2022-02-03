cefc51d01de7f10e17e3ff0f291bab7910e69116
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-03 16:27:45 -0500

    Add files via upload

---
+++ tic-tac-toe/app.js
@@ -0,0 +1,12 @@
+//instantiate UI
+const gameBoard = document.querySelector('.content');
+const cell=document.querySelector('.cell'); //will need a forEach()
+const startBtn= document.getElementById('startButton');
+
+
+
+//create a player construtor
+
+//create a module for the game board
+
+//create a module for displayController
