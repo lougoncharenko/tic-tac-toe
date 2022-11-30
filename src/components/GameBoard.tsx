8bb1595105eedd72feea77fde791be8ce248b50d
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-30 01:56:20 -0500

    x and o appears based on player's turn

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,45 +1,53 @@
-import React, {useState} from 'react';
+import React, {MouseEventHandler, useState} from 'react';
 import './Gameboard.css'
 import { Header } from './Header';
 import { player } from './Player';
 
 export const GameBoard = () => {
     const [message, setMessage] = useState<any>("Player One's Turn")
-    const [mark, setMark] = useState<string>('')
+    const [turn, setTurn] = useState(0);
+    const [data, setData] = useState(['', '', '', '', '', 
+    '', '', '', ''])
     const playerOne = player('Player One', 'X');
     const playerTwo = player('Player Two', 'O');
-    let symbol = '';
-    const placeMark = () => {
-        if (symbol === '') {
-            symbol = playerOne.symbol; 
-            setMark(symbol);
-            setMessage("Player Two's Turn");
-        } else if (symbol === playerOne.symbol) {
-            symbol = playerTwo.symbol;
-            setMark(symbol);
-            setMessage("Player One's Turn")
-        } else if (symbol === playerTwo.symbol) {
-            symbol = playerOne.symbol; 
-            setMark(symbol);
-            setMessage("Player Two's Turn");
+ 
+
+      // Function to draw on the board
+      const draw = (event:any, index:number) => {
+        // Draws only if the position is not taken 
+        // and winner is not decided yet
+        if (data[index - 1] === '') {
+            // Draws X if it's player 1's turn else draws O
+            const current = turn === 0 ? "X" : "O"
+            // Updating the data state
+            data[index - 1] = current;
+            //Drawing on the board
+            event.target.innerText = current;
+            // Switching the turn
+            setTurn(turn === 0 ? 1 : 0)
+            if (turn === 0) {
+                setMessage("Player One's Turn")
+            } else {
+                setMessage("Player One's Turn")
+            }
         }
+      
     }
     return (
     <>
     <Header  text ={message} />
-
     <div className="content">
-        <div className="cell" id="0" onClick = {placeMark}> <p>{mark}</p> </div>
-        <div className="cell" id="1" onClick = {placeMark}> <p>{mark}</p> </div>
-        <div className="cell" id="2"onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="0" onClick={(e) => draw(e, 1)} >  </div>
+        <div className="cell" id="1" onClick={(e) => draw(e, 2)} >  </div>
+        <div className="cell" id="2" onClick={(e) => draw(e, 3)} >  </div>
 
-        <div className="cell" id="3" onClick = {placeMark}> <p>{mark}</p> </div>
-        <div className="cell" id="4" onClick = {placeMark}> <p>{mark}</p> </div>
-        <div className="cell" id="5" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="3" onClick={(e) => draw(e, 4)} >  </div>
+        <div className="cell" id="4" onClick={(e) => draw(e, 5)} >  </div>
+        <div className="cell" id="5" onClick={(e) => draw(e, 6)} >  </div>
         
-        <div className="cell" id="6" onClick = {placeMark}> <p>{mark}</p> </div>
-        <div className="cell" id="7" onClick = {placeMark}> <p>{mark}</p> </div>
-        <div className="cell" id="8" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="6" onClick={(e) => draw(e, 7)} >  </div>
+        <div className="cell" id="7" onClick={(e) => draw(e, 8)} >  </div>
+        <div className="cell" id="8" onClick={(e) => draw(e, 9)} >  </div>
     </div>
     </>
     );
