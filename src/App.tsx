a431dfccca9c6865ad287af01d11e6eb4cb1b728
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 23:46:27 -0500

    imported game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css'
 import { Header } from './components/Header';
+import { GameBoard } from './components/GameBoard';
 
 
 export default function App () {
