ffceb944073ba217816ec67693638821b4c826b0
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-30 00:21:29 -0500

    added interface and prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Header.css'
 
-export const Header = () => {
+interface Props {
+  text: string
+}
+
+export const Header = (props:Props) => {
   return (
     <div className="header">
     <h1>TIC TAC TOE</h1>
     <button className="button" id="resetBtn">Reset Game</button>
-    <h2 id="winner"></h2>
+    <h2 className="winner">{props.text}</h2>
 </div>
   );
 }
