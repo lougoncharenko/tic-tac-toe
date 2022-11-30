d2f64d540cf0a9956d816aea0a394ffaff088bea
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 23:45:45 -0500

    created gameboard

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Gameboard.css'
+
+export const GameBoard = () => {
+  return (
+    <div className="content">
+                    <div className="cell" id="0"></div>
+                    <div className="cell" id="1"></div>
+                    <div className="cell" id="2"></div>
+        
+                    <div className="cell" id="3"></div>
+                    <div className="cell" id="4"></div>
+                    <div className="cell" id="5"></div>
+                    
+                    <div className="cell" id="6"></div>
+                    <div className="cell" id="7"></div>
+                    <div className="cell" id="8"></div>
+                </div>
+  );
+}
+
