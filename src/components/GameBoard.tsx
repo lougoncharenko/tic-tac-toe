b547d63229757498ccca5d0d24c7b5194c94fa1d
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-30 00:41:30 -0500

    added useState for mark

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -5,6 +5,7 @@ import { player } from './Player';
 
 export const GameBoard = () => {
     const [message, setMessage] = useState<any>("Player One's Turn")
+    const [mark, setMark] = useState<string>('')
     const playerOne = player('Player One', 'X');
     const playerTwo = player('Player Two', 'O');
 
@@ -16,19 +17,18 @@ export const GameBoard = () => {
     <Header  text ={message} />
 
     <div className="content">
-        <div className="cell" id="0" onClick = {placeMark}></div>
-        <div className="cell" id="1" onClick = {placeMark}></div>
-        <div className="cell" id="2"onClick = {placeMark}></div>
+        <div className="cell" id="0" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="1" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="2"onClick = {placeMark}> <p>{mark}</p> </div>
 
-        <div className="cell" id="3" onClick = {placeMark}></div>
-        <div className="cell" id="4" onClick = {placeMark}></div>
-        <div className="cell" id="5" onClick = {placeMark}></div>
+        <div className="cell" id="3" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="4" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="5" onClick = {placeMark}> <p>{mark}</p> </div>
         
-        <div className="cell" id="6" onClick = {placeMark}></div>
-        <div className="cell" id="7" onClick = {placeMark}></div>
-        <div className="cell" id="8" onClick = {placeMark}></div>
+        <div className="cell" id="6" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="7" onClick = {placeMark}> <p>{mark}</p> </div>
+        <div className="cell" id="8" onClick = {placeMark}> <p>{mark}</p> </div>
     </div>
-
     </>
     );
 }
