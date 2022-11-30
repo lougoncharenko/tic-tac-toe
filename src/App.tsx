5d3d32783bf70d5f8fbf1639e1853c4cd4d09f8f
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 23:38:21 -0500

    imported header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css'
+import { Header } from './components/Header';
 
 
 export default function App () {
     return(
-        <></>
+        <>
+        <Header />
+        </>
         );
 };
