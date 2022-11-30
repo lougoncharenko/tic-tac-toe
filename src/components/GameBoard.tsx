be4080181dc900d7c868dfac76b6a4a975593871
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-30 02:27:00 -0500

    checks for winners

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,21 +1,17 @@
-import React, {MouseEventHandler, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './Gameboard.css'
 import { Header } from './Header';
-import { player } from './Player';
 
 export const GameBoard = () => {
-    const [message, setMessage] = useState<any>("Player One's Turn")
     const [turn, setTurn] = useState(0);
     const [data, setData] = useState(['', '', '', '', '', 
     '', '', '', ''])
-    const playerOne = player('Player One', 'X');
-    const playerTwo = player('Player Two', 'O');
- 
-
-      // Function to draw on the board
+    const [winner, setWinner] = useState('');
+    const [reset, setReset] = useState(false);
+    const [message, setMessage] = useState<any>("Player One's Turn")
+    
       const draw = (event:any, index:number) => {
         // Draws only if the position is not taken 
-        // and winner is not decided yet
         if (data[index - 1] === '') {
             // Draws X if it's player 1's turn else draws O
             const current = turn === 0 ? "X" : "O"
@@ -25,13 +21,67 @@ export const GameBoard = () => {
             event.target.innerText = current;
             // Switching the turn
             setTurn(turn === 0 ? 1 : 0)
-            if (turn === 0) {
-                setMessage("Player One's Turn")
-            } else {
-                setMessage("Player One's Turn")
-            }
+            //switching message
+            turn == 0? setMessage("Player One's Turn") : setMessage("Player One's Turn") 
         }
-      
+    }
+
+    useEffect(() => {
+        // Checks for the win condition in rows
+        const checkRow = () => {
+            let ans = false;
+            for (let i = 0; i < 9; i += 3) {
+                ans ||= (data[i] === data[i + 1] && 
+                data[i] === data[i + 2] && 
+                data[i] !== '')
+            }
+            return ans;
+        }
+        // Checks for the win condition in cols
+        const checkCol = () => {
+            let ans = false;
+            for (let i = 0; i < 3; i++) {
+                ans ||= (data[i] === data[i + 3] && 
+                data[i] === data[i + 6] && 
+                data[i] !== '')
+            }
+            return ans;
+        }
+        // Checks for the win condition in diagonals
+        const checkDiagonal = () => {
+            return ((data[0] === data[4] && 
+            data[0] === data[8] && data[0] !== '') || 
+            (data[2] === data[4] && data[2] === data[6] && 
+            data[2] !== ''));
+        }
+  
+        // Checks if at all a win condition is present
+        const checkWin = () => {
+            return (checkRow() || checkCol() || checkDiagonal());
+        }
+        // Checks for a tie
+        const checkTie = () => {
+            let count = 0;
+            data.forEach((cell) => {
+                if (cell !== '') {
+                    count++;
+                }
+            })
+            return count === 9;
+        }
+        // Setting the winner in case of a win
+        if (checkWin()) {
+            setWinner(turn === 0 ? "Player 2 Wins!" : 
+            "Player 1 Wins!");
+        } else if (checkTie()) {
+            // Setting the winner to tie in case of a tie
+            setWinner("It's a Tie!");
+        }
+  
+    })
+
+    const resetGame = () => {
+        setReset(true);
     }
     return (
     <>
@@ -49,6 +99,14 @@ export const GameBoard = () => {
         <div className="cell" id="7" onClick={(e) => draw(e, 8)} >  </div>
         <div className="cell" id="8" onClick={(e) => draw(e, 9)} >  </div>
     </div>
+    <div className={`winner ${winner !== '' ? '' : 'shrink'}`}>
+                {/* Display the current winner */}
+                <div className='winner-text'>{winner}</div>
+                {/* Button used to reset the board */}
+                <button onClick={() => resetGame()} >
+                    Reset Board
+                </button>
+            </div>
     </>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,6 @@ export const Header = (props:Props) => {
   return (
     <div className="header">
     <h1>TIC TAC TOE</h1>
-    <button className="button" id="resetBtn">Reset Game</button>
     <h2 className="winner">{props.text}</h2>
 </div>
   );
